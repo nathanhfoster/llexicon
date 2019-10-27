a01d1246dcf09cf19bab3ffcf6dd7d0ed4fe1603
Nathan  Foster <nateinthegame@gmail.com>
2019-10-27 08:14:48 -0700

    PWA

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import storeFactory from "./store";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import LoadingScreen from "./components/LoadingScreen";
-import * as serviceWorker from "./serviceWorker";
-import { getState } from "./store/persist";
+import React, { Suspense } from "react"
+import ReactDOM from "react-dom"
+import storeFactory from "./store"
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import LoadingScreen from "./components/LoadingScreen"
+import * as serviceWorker from "./serviceWorker"
+import { getState } from "./store/persist"
+const { NODE_ENV } = process.env
 
-const initialState = getState();
-const ReduxStore = storeFactory(initialState);
+const initialState = getState()
+const ReduxStore = storeFactory(initialState)
 
 ReactDOM.render(
   <Provider store={ReduxStore}>
@@ -20,6 +21,12 @@ ReactDOM.render(
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
-);
+)
 
-serviceWorker.register();
+console.log("NODE_ENV: ", NODE_ENV)
+
+if (NODE_ENV === "development") {
+  serviceWorker.unregister()
+} else {
+  serviceWorker.register()
+}
