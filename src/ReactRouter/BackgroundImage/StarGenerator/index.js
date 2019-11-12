da4a1f7bc7072c605ae886cc9b90ed1a8646255b
Nathan  Foster <nateinthegame@gmail.com>
2019-11-11 19:27:48 -0800

    Better

--- src/ReactRouter/BackgroundImage/StarGenerator/index.js
+++ src/ReactRouter/BackgroundImage/StarGenerator/index.js
@@ -3,16 +3,27 @@ import PropTypes from "prop-types"
 import { getRandomInt } from "../../../helpers"
 import Star from "../Star"
 
-const StarGenerator = ({ length = 20 }) => {
-  const stars = []
+const StarGenerator = ({ length = 100 }) => {
+  let topPositions = Array.from(new Array(length).keys())
+  let leftPositions = Array.from(new Array(length).keys())
+  let stars = []
+
   for (let i = 0; i < length; i++) {
+    const randomTop = getRandomInt(0, topPositions.length - 1)
+    topPositions.splice(randomTop, 1)
+
+    const randomLeft = getRandomInt(0, leftPositions.length - 1)
+    leftPositions.splice(randomTop, 1)
+
     const fiftyPercentChance = getRandomInt(0, 100) > 50
     const animation = getRandomInt(1, 5)
+
     const props = {
-      top: getRandomInt(0, 100),
-      left: getRandomInt(0, 100),
+      top: randomTop,
+      left: randomLeft,
       animation: fiftyPercentChance ? animation : false
     }
+
     stars.push(<Star key={i} {...props} />)
   }
   return stars
