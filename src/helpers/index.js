cf3987ab828b307ac06ba4d703bf5ce52b27545d
Nathan Foster <nateinthegame@gmail.com>
2019-11-14 20:34:22 -0800

    Update index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -134,7 +134,7 @@ const getFileFromBase64 = (dataurl, filename) => {
   while (n--) {
     u8arr[n] = bstr.charCodeAt(n)
   }
-  return new File([u8arr], filename, { type: mime })
+  return new File([u8arr], filename.concat(mime), { type: mime })
 }
 
 const joinStrings = objectArray => {
