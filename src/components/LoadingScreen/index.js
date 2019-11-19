61093db8f76cf4cb1594798ebb44251debb335b0
Nathan  Foster <nateinthegame@gmail.com>
2019-11-18 21:56:22 -0800

    Updated picture

--- src/components/LoadingScreen/index.js
+++ src/components/LoadingScreen/index.js
@@ -3,8 +3,10 @@ import React, { memo } from "react"
 import "./styles.css"
 
 const LoadingScreen = ({ title = "Loading..." }) => (
-  <div className="loader">
-    <i className="fas fa-feather-alt" style={{ fontSize: 18}} /> {title}
+  <div className="LoadingScreenContainer">
+    <div className="loader">
+      <i className="fas fa-feather-alt" style={{ fontSize: 18 }} /> {title}
+    </div>
   </div>
 )
 
