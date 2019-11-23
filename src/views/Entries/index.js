b20ebcb9e6deb16e63383a730211315c0ea18348
Nathan Foster <nateinthegame@gmail.com>
2019-11-23 15:21:06 -0800

    Update index.js

--- src/views/Entries/index.js
+++ src/views/Entries/index.js
@@ -133,11 +133,11 @@ class Entries extends Component {
     const { listView, minXs, minMd, minLg, minXl } = this.state
     return entries.map(entry => {
       return listView ? (
-        <Col key={entry.id} xs={12} className="p-0" >
+        <Col key={entry.id} xs={12} >
           <EntryMinimal {...entry} />
         </Col>
       ) : (
-        <Col key={entry.id} xs={minXs} md={minMd} lg={minLg} xl={minXl} className="p-0" >
+        <Col key={entry.id} xs={minXs} md={minMd} lg={minLg} xl={minXl} >
           <EntryMinimal {...entry} />
         </Col>
       )
@@ -153,7 +153,6 @@ class Entries extends Component {
         key={id}
         style={{ ...style /* background: "red" */ }}
         xs={12}
-        className="p-0"
       >
         <Entry
           key={id}
@@ -180,7 +179,7 @@ class Entries extends Component {
     } = this.state
 
     return entries.length > 0 ? (
-      <Container className="Entries Container p-0">
+      <Container className="Entries Container">
         <Nav tabs>
           <NavItem>
             <NavLink
