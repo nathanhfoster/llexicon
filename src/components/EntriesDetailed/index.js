058b7026b3b594b6f32c9b9c898bb92f56e81798
Nathan  Foster <nateinthegame@gmail.com>
2020-02-06 10:56:29 -0800

    FixedSizeList Render
    FixedSizeList render method needs to be declared outside a functional component

--- src/components/EntriesDetailed/index.js
+++ src/components/EntriesDetailed/index.js
@@ -1,74 +1,76 @@
-import React, { Component, createRef } from "react"
+import React, { useRef, memo } from "react"
 import PropTypes from "prop-types"
 import { Col } from "reactstrap"
 import { FixedSizeList } from "react-window"
 import Entry from "../../components/Entry"
 import deepEquals from "../../helpers/deepEquals"
 
-class EntriesDetailed extends Component {
-  constructor(props) {
-    super(props)
+const renderDetailedEntries = ({ data, index, style, isScrolling }) => {
+  const entry = data[index]
 
-    this.detailedEntriesListRef = createRef()
-
-    this.state = {}
-  }
-
-  static propTypes = {
-    entries: PropTypes.arrayOf(PropTypes.object.isRequired),
-    onItemsRendered: PropTypes.func,
-    height: PropTypes.number.isRequired,
-    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-  }
-
-  static defaultProps = {
-    height: 500,
-    width: "100%",
-    itemSize: 60
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    const propsChanged = !deepEquals(this.props, nextProps)
-
-    return propsChanged
-  }
-
-  renderDetailedEntries = ({ data, index, style, isScrolling }) => {
-    const entry = data[index]
-
-    return (
-      <Col
-        key={entry.id}
-        style={{ ...style /* background: "red" */ }}
-        xs={12}
-        className="p-0"
-      >
-        <Entry
-          entry={entry}
-          containerHeight={style.height}
-          bottomToolbarHidden
-          theme="snow"
-        />
-      </Col>
-    )
-  }
-
-  render() {
-    const { entries, onItemsRendered, height, width, itemSize } = this.props
-    
-    return (
-      <FixedSizeList
-        ref={this.detailedEntriesListRef}
-        height={height}
-        width={width}
-        itemData={entries}
-        itemCount={entries.length}
-        itemSize={itemSize}
-        onItemsRendered={onItemsRendered}
-      >
-        {this.renderDetailedEntries}
-      </FixedSizeList>
-    )
-  }
+  return (
+    <Col
+      key={entry.id}
+      style={{ ...style /* background: "red" */ }}
+      xs={12}
+      className="p-0"
+    >
+      <Entry
+        entry={entry}
+        containerHeight={style.height}
+        bottomToolbarHidden
+        theme="snow"
+      />
+    </Col>
+  )
 }
-export default EntriesDetailed
+
+const EntriesDetailed = ({
+  entries,
+  onItemsRendered,
+  height,
+  width,
+  itemSize
+}) => {
+  const detailedEntriesListRef = useRef()
+
+  return (
+    <FixedSizeList
+      ref={detailedEntriesListRef}
+      height={height}
+      width={width}
+      itemData={entries}
+      itemCount={entries.length}
+      itemSize={itemSize}
+      onItemsRendered={onItemsRendered}
+    >
+      {renderDetailedEntries}
+    </FixedSizeList>
+  )
+}
+
+EntriesDetailed.propTypes = {
+  entries: PropTypes.arrayOf(PropTypes.object.isRequired),
+  onItemsRendered: PropTypes.func,
+  height: PropTypes.number.isRequired,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+}
+
+EntriesDetailed.defaultProps = {
+  height: 500,
+  width: "100%",
+  itemSize: 60
+}
+
+const isEqual = (prevProps, nextProps) => {
+  const memoProps = ["entries", "height", "itemSize", "width"]
+  for (let i = 0, { length } = memoProps; i < length; i++) {
+    const prop = memoProps[i]
+    if (!deepEquals(prevProps[prop], nextProps[prop])) {
+      return false
+    }
+  }
+  return true
+}
+
+export default memo(EntriesDetailed, isEqual)
