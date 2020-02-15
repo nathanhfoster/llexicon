0e6b0fa8d72898bb0cc52f5ad5d2aac8f09e5f77
Nathan  Foster <nateinthegame@gmail.com>
2020-02-15 12:22:13 -0800

    Fixed

--- src/helpers/memoizeProps.js
+++ src/helpers/memoizeProps.js
@@ -4,11 +4,11 @@ const memoizeProps = (prevProps, nextProps, memoProps) => {
   for (let i = 0, { length } = memoProps; i < length; i++) {
     const prop = memoProps[i]
     if (!deepEquals(prevProps[prop], nextProps[prop])) {
-      return true
+      return false
     }
   }
 
-  return false
+  return true
 }
 
 export default memoizeProps
