c3a82571710e17de20c2abfae4d2ed15f214d4fe
Nathan  Foster <nateinthegame@gmail.com>
2020-02-21 12:05:11 -0800

    New Loading Screen

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ import storeFactory from "./redux"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import { getReduxState } from "./redux/Persister/actions"
+import { getRandomInt } from "./helpers"
 import * as serviceWorker from "./serviceWorker"
 
 const AlertNotifications = lazy(() => import("./components/AlertNotifications"))
 const Persister = lazy(() => import("./components/Persister"))
-const BackgroundImage = lazy(() => import("./components/BackgroundImage"))
+const BackgroundImage = lazy(() => {
+  return new Promise(resolve =>
+    setTimeout(resolve, getRandomInt(1600, 3200))
+  ).then(
+    () => import("./components/BackgroundImage")
+    // : Promise.reject(new Error())
+  )
+})
 
 const { NODE_ENV } = process.env
 
