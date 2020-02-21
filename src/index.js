5083f0b616c7d24ab672a90f10b7c88062198b7b
Nathan  Foster <nateinthegame@gmail.com>
2020-02-21 10:39:53 -0800

    Better

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import "./styles/index.css"
 import React, { Suspense, lazy } from "react"
 import ReactDOM from "react-dom"
 import { LoadingScreen, NavBar } from "./components"
+import App from "./App"
 import storeFactory from "./redux"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import AlertNotifications from "./components/AlertNotifications"
-import Persister from "./components/Persister"
 import { getReduxState } from "./redux/Persister/actions"
 import * as serviceWorker from "./serviceWorker"
 
+const AlertNotifications = lazy(() => import("./components/AlertNotifications"))
+const Persister = lazy(() => import("./components/Persister"))
 const BackgroundImage = lazy(() => import("./components/BackgroundImage"))
 
 const { NODE_ENV } = process.env
