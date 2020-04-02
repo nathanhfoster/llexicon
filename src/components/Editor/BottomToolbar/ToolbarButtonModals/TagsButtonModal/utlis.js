1422101f4230a7a56653a22743a6ee123d3d1904
Nathan Foster <nateinthegame@gmail.com>
2020-04-02 09:50:23 -0700

    Tags Modal now filters out existing tags

---
+++ src/components/Editor/BottomToolbar/ToolbarButtonModals/TagsButtonModal/utlis.js
@@ -0,0 +1,10 @@
+import { removeArrayDuplicates } from "../../../../../helpers"
+const validatedString = s => {
+  const validatedString = s.replace(/[^A-Z0-9]+/gi, " ")
+  const filteredString = removeArrayDuplicates(validatedString.split(" ")).join(
+    " "
+  )
+  return filteredString
+}
+
+export { validatedString }
