237010bd93ef2f0d466575d1946d5a60fa710e22
Nathan Foster <nateinthegame@gmail.com>
2020-04-07 02:12:12 -0700

    Better Table

--- src/components/BasicTable/TableBody/index.js
+++ src/components/BasicTable/TableBody/index.js
@@ -1,36 +1,38 @@
-import React, { memo } from "react"
+import React, { useMemo, memo } from "react"
 import PropTypes from "prop-types"
 import { ColumnsPropType, DataPropType } from "../propTypes"
 import "./styles.css"
 
-const renderTableRows = (columns, data, onRowClick) =>
-  data.map((item, i) => {
-    const [firstColumn, ...restOfColumns] = columns
-    const { title, dataIndex, key, width = "auto", render } = firstColumn
-    const firstItemIndexOrKeyValue = item[dataIndex || key]
+const TableBody = ({ columns, data, onRowClick }) => {
+  const renderTableRows = useMemo(
+    () =>
+      data.map((item, i) => {
+        const [firstColumn, ...restOfColumns] = columns
+        const { title, dataIndex, key, width = "auto", render } = firstColumn
+        const firstItemIndexOrKeyValue = item[dataIndex || key]
 
-    return (
-      <tr key={i} onClick={onRowClick ? () => onRowClick(item) : null}>
-        <th scope="row" style={{ fontWeight: "normal" }}>
-          {render ? render(item) : firstItemIndexOrKeyValue}
-        </th>
-        {restOfColumns.map((c, j) => {
-          const { title, dataIndex, key, width = "auto", render } = c
-          const itemValue = item[dataIndex || key]
-          return <td key={j}>{render ? render(item) : itemValue}</td>
-        })}
-      </tr>
-    )
-  })
-
-const TableBody = ({ columns, data, onRowClick }) => (
-  <tbody>{renderTableRows(columns, data, onRowClick)}</tbody>
-)
+        return (
+          <tr key={i} onClick={onRowClick ? () => onRowClick(item) : null}>
+            <th scope="row" style={{ fontWeight: "normal" }}>
+              {render ? render(item) : firstItemIndexOrKeyValue}
+            </th>
+            {restOfColumns.map((c, j) => {
+              const { title, dataIndex, key, width = "auto", render } = c
+              const itemValue = item[dataIndex || key]
+              return <td key={j}>{render ? render(item) : itemValue}</td>
+            })}
+          </tr>
+        )
+      }),
+    [columns, data]
+  )
+  return <tbody>{renderTableRows}</tbody>
+}
 
 TableBody.propTypes = {
   onRowClick: PropTypes.func,
   columns: ColumnsPropType,
-  data: DataPropType
+  data: DataPropType,
 }
 
 export default memo(TableBody)
--- src/components/BasicTable/TableFooter/index.js
+++ src/components/BasicTable/TableFooter/index.js
@@ -3,24 +3,27 @@ import PropTypes from "prop-types"
 import { ColumnsPropType, DataPropType } from "../propTypes"
 import "./styles.css"
 
-const renderTableRows = (columns, sortedAndFilteredData, onRowClick) =>
-  columns.map((column, i) => {
-    const { footer } = column
-
-    return footer ? (
-      <td key={i}>{footer(sortedAndFilteredData)}</td>
-    ) : (
-      <td key={i}></td>
-    )
-  })
-
 const TableFooter = ({ columns, sortedAndFilteredData, onRowClick }) => {
-  const shouldRender = useMemo(() => columns.some(column => column.footer))
+  const shouldRender = useMemo(() => columns.some((column) => column.footer))
+
+  const renderTableRows = useMemo(
+    () =>
+      columns.map((column, i) => {
+        const { footer } = column
+
+        return footer ? (
+          <td key={i}>{footer(sortedAndFilteredData)}</td>
+        ) : (
+          <td key={i}></td>
+        )
+      }),
+    [columns, sortedAndFilteredData]
+  )
 
   return (
     shouldRender && (
       <tfoot>
-        <tr>{renderTableRows(columns, sortedAndFilteredData, onRowClick)}</tr>
+        <tr>{renderTableRows}</tr>
       </tfoot>
     )
   )
@@ -29,7 +32,7 @@ const TableFooter = ({ columns, sortedAndFilteredData, onRowClick }) => {
 TableFooter.propTypes = {
   onRowClick: PropTypes.func,
   columns: ColumnsPropType,
-  sortedAndFilteredData: DataPropType
+  sortedAndFilteredData: DataPropType,
 }
 
 export default memo(TableFooter)
