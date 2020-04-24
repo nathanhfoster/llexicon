92c1b4591625b613e7c864df6a3c5a7e44f45e12
Nathan Foster <nateinthegame@gmail.com>
2020-04-24 14:44:47 -0700

    Update reducer.js

--- src/redux/TextEditor/reducer.js
+++ src/redux/TextEditor/reducer.js
@@ -44,7 +44,7 @@ const TextEditor = (state = DEFAULT_STATE_TEXT_EDITOR, action) => {
       return { ...DEFAULT_STATE_TEXT_EDITOR, tags: [], EntryFiles: [], rating: 0, clearedOn: new Date() }
 
     case AppActionTypes.REDUX_RESET:
-      return { ...DEFAULT_STATE_TEXT_EDITOR, clearedOn: new Date() }
+      return { ...DEFAULT_STATE_TEXT_EDITOR, tags: [], EntryFiles: [], rating: 0, clearedOn: new Date() }
 
     default:
       return state
