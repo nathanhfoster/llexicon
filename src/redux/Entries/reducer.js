1536efe8ce0ce8a61ea0a29eb2967a5ff483d780
Nathan <nateinthegame@gmail.com>
2020-04-26 12:39:53 -0700

    B

--- src/redux/Entries/reducer.js
+++ src/redux/Entries/reducer.js
@@ -30,7 +30,11 @@ const defaultEntry = {
       name: "Inspired",
     },
   ],
-  people: [],
+  people: [
+    {
+      name: "Me",
+    },
+  ],
   EntryFiles: DEFAULT_ENTRY_FILES,
   title: "My First Journal Entry",
   html: `<p class="ql-align-center"><img src="${entryFiles.Logo}" width="140"></p><br><p>After I've installed Astral Tree today, I will make a diary entry every day from now on. In case I forget to make an entry, the app will remind me with a notification in the evening. Besides pictures, videos, audio recordings or other files, I can add a location, tags or people to my journal entries.</p><p><br></p><p>If I <a href="${LINK_TO_SIGN_UP}" rel="noopener noreferrer" target="_blank">sign up</a>, my journal entries will be synced across all my devices. I am already looking forward to revisiting all those memories in a few months or years.</p>`,
@@ -63,7 +67,29 @@ const DEFAULT_STATE_ENTRIES = {
   isPending: false,
   error: null,
   search: "",
-  EntryTags: [],
+  EntryTags: [
+    {
+      name: "Family",
+    },
+    {
+      name: "Friends",
+    },
+    {
+      name: "Document",
+    },
+    {
+      name: "Link",
+    },
+    {
+      name: "Music",
+    },
+    {
+      name: "Vacation",
+    },
+    {
+      name: "Video",
+    },
+  ],
   EntryPeople: [],
   sortMap: {
     date_updated: true,
