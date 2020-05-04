4aa94183f142efd370df223e41a3281085a0172d
Nathan <nateinthegame@gmail.com>
2020-05-04 15:30:34 -0700

    B

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -401,7 +401,6 @@ const copyStringToClipboard = (s) => {
   el.value = s
   // Set non-editable to avoid focus and move outside of view
   el.setAttribute("readonly", "")
-  el.style = { position: "absolute", left: "-9999px" }
   document.body.appendChild(el)
   // Select text inside element
   el.select()
