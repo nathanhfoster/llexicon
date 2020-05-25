0df607f4d54091585c1aeb7419028f4e0ac5f202
Nathan <nateinthegame@gmail.com>
2020-05-24 20:56:01 -0700

    Context doesn't have apply middleware or thunk

--- src/store/provider/connect.js
+++ src/store/provider/connect.js
@@ -1,6 +1,72 @@
 import * as React from "react"
 import { ContextConsumer } from "./provider"
 
+// ƒ () {
+//   return dispatch(actionCreator.apply(this, arguments));
+// }
+
+const thunk = (extraArgument) => {
+  return ({ dispatch, getState }) => (next) => (action) => {
+    if (typeof action === "function") {
+      return action(dispatch, getState, extraArgument)
+    }
+
+    return next(action)
+  }
+}
+
+const compose = (...funcs) => {
+  if (funcs.length === 0) {
+    return (arg) => arg
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
+
+const applyMiddleware = (...middlewares) => {
+  return (createStore) => (...args) => {
+    const store = createStore(...args)
+    let dispatch = () => {
+      throw new Error(
+        "Dispatching while constructing your middleware is not allowed. " +
+          "Other middleware would not be applied to this dispatch."
+      )
+    }
+
+    const middlewareAPI = {
+      getState: store.getState,
+      dispatch: (...args) => dispatch(...args),
+    }
+    const chain = middlewares.map((middleware) => middleware(middlewareAPI))
+    dispatch = compose(...chain)(store.dispatch)
+
+    return {
+      ...store,
+      dispatch,
+    }
+  }
+}
+
+const wrapDispatchAndState = (mapDispatchToProps, dispatch, state) => {
+  console.log(mapDispatchToProps)
+  return Object.keys(mapDispatchToProps).reduce((dispatchToProps, key) => {
+    const propAction = dispatchToProps[key]
+    console.log(propAction)
+    // console.log(propAction)
+    // if (propAction instanceof Function || typeof propAction === "function") {
+    //   dispatchToProps[key] = (...args) => {
+    //     console.log(args)
+    //     return propAction((dispatch, state))
+    //   }
+    // }
+    return dispatchToProps
+  }, mapDispatchToProps)
+}
+
 /**
  * function in charge of combining the factories, props and context to a React.Component
  *
@@ -23,13 +89,17 @@ const connect = (mapStateToProps, mapDispatchToProps) => {
     return (props) => {
       return (
         <ContextConsumer.Consumer>
-          {(value) => {
-            const stateToProps = mapStateToProps(value.state)
+          {({ state, dispatch }) => {
+            const stateToProps = mapStateToProps(state)
 
-            const dispatchToProps = mapDispatchToProps
+            const dispatchToProps = !mapDispatchToProps
+              ? null
+              : mapDispatchToProps instanceof Function ||
+                typeof mapDispatchToProps === "function"
               ? // the dispatch provided by the consumer; our global reducer
-                mapDispatchToProps(value.dispatch)
-              : null
+                mapDispatchToProps(dispatch, state)
+              : // wrap the dispatch and state
+                wrapDispatchAndState(mapDispatchToProps, dispatch, state)
 
             const componentProps = {
               ...stateToProps,
---
+++ src/store/provider/index.js
@@ -0,0 +1,11 @@
+import connect from "./connect"
+import { ContextProvider, ContextConsumer } from "./provider"
+import { defaultInitializer, useProviderReducer } from "./useProviderReducer"
+
+export {
+  connect,
+  ContextProvider,
+  ContextConsumer,
+  defaultInitializer,
+  useProviderReducer,
+}
