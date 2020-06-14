30edc534b503d0b47b270e59edeb5542c955d63e
Nathan Foster <nateinthegame@gmail.com>
2020-06-14 12:56:08 -0700

    useScrollable

--- src/components/EntryComponents/EntryCards/index.js
+++ src/components/EntryComponents/EntryCards/index.js
@@ -1,14 +1,15 @@
 import React, { useMemo, memo } from "react"
+import PropTypes from "prop-types"
 import { EntriesPropTypes } from "../../../redux/Entries/propTypes"
 import { Col } from "reactstrap"
 import EntryCard from "./EntryCard"
 
-const EntryCards = ({ className, entries }) => {
+const EntryCards = ({ className, entries, minimal }) => {
   const renderEntryCards = useMemo(
     () =>
       entries.map((entry) => (
         <Col key={entry.id} xs={6} md={4} xl={3} className={className}>
-          <EntryCard {...entry} />
+          <EntryCard {...entry} minimal={minimal} />
         </Col>
       )),
     [entries]
@@ -17,8 +18,12 @@ const EntryCards = ({ className, entries }) => {
   return renderEntryCards
 }
 
-EntryCards.propTypes = { entries: EntriesPropTypes }
+EntryCards.propTypes = {
+  className: PropTypes.string.isRequired,
+  entries: EntriesPropTypes,
+  minimal: PropTypes.bool.isRequired,
+}
 
-EntryCards.defaultProps = { className: "fade-in p-1 p-sm-2" }
+EntryCards.defaultProps = { className: "fade-in p-1 p-sm-2", minimal: false }
 
 export default memo(EntryCards)
