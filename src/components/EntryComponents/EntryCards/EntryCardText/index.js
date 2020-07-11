c03f41cb4f0101234a5a544f65bf30d098eabcbc
Nathan Foster <nateinthegame@gmail.com>
2020-07-11 03:02:18 -0700

    Update index.js

--- src/components/EntryComponents/EntryCards/EntryCardText/index.js
+++ src/components/EntryComponents/EntryCards/EntryCardText/index.js
@@ -36,10 +36,7 @@ const EntryCardText = ({
         </Col>
       </Row>
       <Row>
-        <Col xs={4} className="EntryCardTextLeftColumn p-0">
-          <i className="far fa-eye" /> <span className="mr-2">{views}</span>
-        </Col>
-        <Col xs={8} className="EntryCardTextRightColumn p-0">
+        <Col xs={12} className="EntryCardTextRightColumn p-0">
           <RatingIcon rating={rating} />
         </Col>
       </Row>
@@ -52,9 +49,12 @@ const EntryCardText = ({
           <i className="fas fa-pencil-alt mr-1" />
           <Moment fromNow>{date_updated}</Moment>
         </Col>
-        <Col xs={12} className="EntryCardTextLeftColumn p-0">
+        <Col xs={8} className="EntryCardTextLeftColumn p-0">
           <i className="fas fa-hdd mr-1" />
           {formatBytes(size)}
+        </Col>
+        <Col xs={4} className="EntryCardTextLeftColumn p-0">
+          <i className="far fa-eye" /> <span>{views}</span>
         </Col>
       </Row>
     </Container>
