d4fcc96ff5c22319614f3289926bdc4a6b50aa27
Nathan <nateinthegame@gmail.com>
2020-07-14 13:30:32 -0700

    B

--- src/views/Admin/index.js
+++ src/views/Admin/index.js
@@ -63,6 +63,18 @@ const Admin = ({ isPending, users, GetAllUsers, GetAllUserEntries }) => {
         title: <i className="fas fa-id-card" />,
         key: "username",
         width: 120,
+        render: ({ id, username }) => (
+          <a
+            onClick={(e) => e.stopPropagation()}
+            href={REACT_APP_API_URL.replace(
+              "api/v1/",
+              `user/user/${id}/change/`
+            )}
+            target="_blank"
+          >
+            {username}
+          </a>
+        ),
         footer: (items) =>
           items.reduce((count, { username }) => (count + username ? 1 : 0), 0),
       },
