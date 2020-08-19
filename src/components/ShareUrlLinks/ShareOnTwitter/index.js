485183a5f5c0cac6d07d17808b523f6fcda77d56
Nathan Foster <nathan.foster@passportinc.com>
2020-08-19 10:46:36 -0700

    U

--- src/components/ShareUrlLinks/ShareOnTwitter/index.js
+++ src/components/ShareUrlLinks/ShareOnTwitter/index.js
@@ -2,17 +2,19 @@ import React, { memo } from "react"
 import PropTypes from "prop-types"
 import ShareUrl from "../"
 
-const ShareOnTwitter = ({ text }) => (
-  <ShareUrl href={`https://twitter.com/intent/tweet?text=${text}`}>
+const ShareOnTwitter = ({ text, ...restOfProps }) => (
+  <ShareUrl href={`https://twitter.com/intent/tweet?text=${text}`} {...restOfProps}>
     <i className="fab fa-twitter-square" />
   </ShareUrl>
 )
 
 ShareOnTwitter.propTypes = {
+  title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
 }
 
 ShareOnTwitter.defaultProps = {
+  title: 'Twitter',
   text: `Check my journal entry: ${window.location.href}`,
 }
 
--- src/components/ShareUrlLinks/index.js
+++ src/components/ShareUrlLinks/index.js
@@ -1,13 +1,24 @@
 import React, { memo } from "react"
 import PropTypes from "prop-types"
 
-const ShareUrl = ({ children, ...restOfProps }) => (
-  <a {...restOfProps} onClick={(e) => e.stopPropagation()}>
+const ShareUrl = ({ children, ...restOfProps }) => 'share' in navigator ? (
+  <span onClick={(e) => {
+    e.stopPropagation();
+    navigator.share({
+      title: restOfProps.title,
+      text: restOfProps.text,
+      url: restOfProps.url
+    })
+  }
+  } {...restOfProps}>{children}</span >
+) : <a {...restOfProps} onClick={(e) => e.stopPropagation()}>
     {children}
   </a>
-)
+
 
 ShareUrl.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
   href: PropTypes.string.isRequired,
   target: PropTypes.oneOf(["_self", "_blank", "_parent", "_top"]),
   children: PropTypes.oneOfType([
