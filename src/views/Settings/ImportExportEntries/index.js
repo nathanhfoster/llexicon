73d13f7feb900f8a6785ccbeae32acd47446bded
Nathan Foster <nathan.foster@passportinc.com>
2020-10-04 13:26:19 -0700

    GetAllUserEntryPages

--- src/views/Settings/ImportExportEntries/index.js
+++ src/views/Settings/ImportExportEntries/index.js
@@ -1,36 +1,39 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { EntriesPropTypes } from "../../../redux/Entries/propTypes"
-import { Container, Row, Col, ButtonGroup, Button } from "reactstrap"
-import { ImportEntries } from "../../../components"
-import { connect as reduxConnect } from "react-redux"
-import { copyStringToClipboard } from "../../../utils"
-import { SyncEntries, GetAllUserEntries } from "../../../redux/Entries/actions"
-import MomentJs from "moment"
+import React, { useCallback } from 'react'
+import PropTypes from 'prop-types'
+import { EntriesPropTypes } from '../../../redux/Entries/propTypes'
+import { Container, Row, Col, ButtonGroup, Button } from 'reactstrap'
+import { ImportEntries } from '../../../components'
+import { connect as reduxConnect } from 'react-redux'
+import { copyStringToClipboard } from '../../../utils'
+import {
+  SyncEntries,
+  GetAllUserEntries,
+  GetAllUserEntryPages,
+} from '../../../redux/Entries/actions'
+import MomentJs from 'moment'
 
-const mapStateToProps = ({
-  User: { id },
-  Entries: { items, filteredItems },
-}) => ({
+const mapStateToProps = ({ User: { id }, Entries: { items, filteredItems } }) => ({
   userIsLoggedIn: !!id,
   items,
   filteredItems,
 })
 
-const mapDispatchToProps = { SyncEntries, GetAllUserEntries }
+const mapDispatchToProps = { SyncEntries, GetAllUserEntryPages }
 
 const ImportExportEntries = ({
   userIsLoggedIn,
   items,
   filteredItems,
   SyncEntries,
-  GetAllUserEntries,
+  GetAllUserEntryPages,
 }) => {
   const entries = items.concat(filteredItems)
   const totalEntries = entries.length
 
-  const GetAllEntries = () =>
-    SyncEntries(() => new Promise((resolve) => resolve(GetAllUserEntries())))
+  const GetAllEntries = useCallback(
+    () => SyncEntries(() => new Promise(resolve => resolve(GetAllUserEntryPages()))),
+    [],
+  )
 
   const handleExportEntries = () => {
     const formattedEntries = entries.map((entry, i) => {
@@ -48,25 +51,17 @@ const ImportExportEntries = ({
         latitude,
         longitude,
       } = entry
-      const dateFormat = "YYYY-MM-DD hh:mm:ss"
+      const dateFormat = 'YYYY-MM-DD hh:mm:ss'
 
       return {
         id,
         author,
-        tags: tags.reduce(
-          (tagString, tag) => (tagString += `${tag.name},`),
-          ""
-        ),
-        people: people.reduce(
-          (peopleString, person) => (peopleString += `${person.name},`),
-          ""
-        ),
+        tags: tags.reduce((tagString, tag) => (tagString += `${tag.name},`), ''),
+        people: people.reduce((peopleString, person) => (peopleString += `${person.name},`), ''),
         title,
         html,
         date_created: MomentJs(date_created).format(dateFormat),
-        date_created_by_author: MomentJs(date_created_by_author).format(
-          dateFormat
-        ),
+        date_created_by_author: MomentJs(date_created_by_author).format(dateFormat),
         date_updated: MomentJs(date_updated).format(dateFormat),
         views,
         latitude,
@@ -74,28 +69,19 @@ const ImportExportEntries = ({
       }
     })
     copyStringToClipboard(JSON.stringify(formattedEntries))
-    alert("Entries copied to clipboard as a JSON.")
+    alert('Entries copied to clipboard as a JSON.')
   }
 
   return (
     <Container fluid>
-      <Row className="py-2">
-        <Col xs={12} tag={ButtonGroup} className="p-0">
-          <Button
-            color="accent"
-            onClick={GetAllEntries}
-            disabled={!userIsLoggedIn}
-          >
-            <i className="fas fa-cloud-download-alt" /> Download and Sync All
-            Entries
+      <Row className='py-2'>
+        <Col xs={12} tag={ButtonGroup} className='p-0'>
+          <Button color='accent' onClick={GetAllEntries} disabled={!userIsLoggedIn}>
+            <i className='fas fa-cloud-download-alt' /> Download and Sync All Entries
           </Button>
           <ImportEntries />
-          <Button
-            color="accent"
-            onClick={handleExportEntries}
-            disabled={totalEntries === 0}
-          >
-            <i className="fas fa-clipboard" /> Export Entries
+          <Button color='accent' onClick={handleExportEntries} disabled={totalEntries === 0}>
+            <i className='fas fa-clipboard' /> Export Entries
           </Button>
         </Col>
       </Row>
@@ -111,7 +97,4 @@ ImportExportEntries.propTypes = {
 
 ImportExportEntries.defaultProps = { userIsLoggedIn: false }
 
-export default reduxConnect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ImportExportEntries)
+export default reduxConnect(mapStateToProps, mapDispatchToProps)(ImportExportEntries)
