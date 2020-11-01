f72f53c0391a7f40ada97e3fedfd32311b612bf9
Nathan Foster <nateinthegame@gmail.com>
2020-11-01 07:08:17 -0800

    Update index.js

--- src/components/EntryComponents/EntriesMostViewed/index.js
+++ src/components/EntryComponents/EntriesMostViewed/index.js
@@ -2,6 +2,7 @@ import React, { useRef, useMemo, Fragment } from 'react'
 import { EntriesPropTypes } from 'redux/Entries/propTypes'
 import { connect } from 'react-redux'
 import { EntryCards, Header } from '../..'
+import { Col } from 'reactstrap'
 
 const mapStateToProps = ({ Entries: { items, filteredItems, showOnlyPublic } }) => ({
   items,
@@ -22,7 +23,9 @@ const EntriesMostViewed = ({ items, filteredItems, showOnlyPublic }) => {
 
   return (
     <Fragment>
-      <Header fill='var(--quinaryColor)'>Most Viewed Entries</Header>
+      <Col xs={12} className='p-0'>
+         <Header fill='var(--quinaryColor)'>Most Viewed Entries</Header>
+      </Col>
       <div ref={containerRef} className='HomeRow mb-3 pb-1 row'>
         <EntryCards entries={entriesMostViewed} containerRef={containerRef} />
       </div>
