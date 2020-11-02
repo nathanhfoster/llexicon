6030c0ae5514ba50bd021c26bc264c3f197ad0b4
Nathan Foster <nathan.foster@passportinc.com>
2020-11-01 18:28:54 -0800

    B

--- src/components/BasicComponents/BasicTable/TableFooters/index.js
+++ src/components/BasicComponents/BasicTable/TableFooters/index.js
@@ -27,7 +27,7 @@ const TableFooters = ({ columns, sortedAndFilteredData, selectedData, actionMenu
   return (
     shouldRender && (
       <tfoot className='BasicTableFooter'>
-        {actionMenuCallback && <tr className='text-center'>{selectedData.length}</tr>}
+        {actionMenuCallback && <tr className='text-center'g>{selectedData.length}</tr>}
         <tr>{renderTableRows}</tr>
       </tfoot>
     )
