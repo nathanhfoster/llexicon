3d429da6644d5ab7a6af4d15d528176992604d65
Nathan Foster <nateinthegame@gmail.com>
2021-01-10 10:14:32 -0800

    Update index.js

--- src/views/EntryDetail/index.js
+++ src/views/EntryDetail/index.js
@@ -32,7 +32,7 @@ const mapStateToProps = (
   { entryId },
 ) => ({
   userId: id,
-  entry: item || items.concat(filteredItems).find(({ id }) => id == entryId),
+  entry: item?.id ? item : items.concat(filteredItems).find(({ id }) => id == entryId),
   serviceWorkerController: serviceWorker?.controller || {},
   isPending,
 })
