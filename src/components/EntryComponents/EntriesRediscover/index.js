9cd1294ddd8bc1f90a9d8e4349501a9b87e6629c
Nathan Foster <nfoster@rnaker.com>
2021-10-07 17:52:02 -0700

    Fix

--- src/components/EntryComponents/EntriesRediscover/index.js
+++ src/components/EntryComponents/EntriesRediscover/index.js
@@ -1,43 +1,54 @@
-import React, { useRef, useMemo, Fragment } from 'react'
-import PropTypes from 'prop-types'
-import { EntriesPropTypes } from 'redux/Entries/propTypes'
-import { connect } from 'react-redux'
-import { EntryCards, Header } from '../..'
-import Moment from 'react-moment'
-import { Col } from 'reactstrap'
-import { RouteMap } from 'redux/router/actions'
+import React, { useRef, useMemo, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { EntriesPropTypes } from 'redux/Entries/propTypes';
+import { connect } from 'react-redux';
+import { EntryCards, Header } from '../..';
+import Moment from 'react-moment';
+import { Col } from 'reactstrap';
+import { RouteMap } from 'redux/router/actions';
 
-const mapStateToProps = ({ Entries: { items, filteredItems, showOnlyPublic } }) => ({
+const mapStateToProps = ({
+  Entries: { items, filteredItems, showOnlyPublic }
+}) => ({
   entries: filteredItems.length > 0 ? items.concat(filteredItems) : items,
-  showOnlyPublic,
-})
+  showOnlyPublic
+});
 
-export const EntriesRediscover = ({ entries, showOnlyPublic, height, headerLink }) => {
-  const containerRef = useRef()
-  const today = new Date()
-  const styles = useMemo(() => ({ maxHeight: height }), [height])
+export const EntriesRediscover = ({
+  entries,
+  showOnlyPublic,
+  height,
+  headerLink
+}) => {
+  const containerRef = useRef();
+  const today = new Date();
+  const styles = useMemo(() => ({ maxHeight: height }), [height]);
   const entriesOnThisDay = useMemo(
     () =>
       entries
         .filter(({ date_created_by_author, _shouldDelete, is_public }) => {
-          if (_shouldDelete || is_public !== showOnlyPublic) return false
-          const entryDate = new Date(date_created_by_author)
+          if (_shouldDelete || is_public !== showOnlyPublic) return false;
+          const entryDate = new Date(date_created_by_author);
           const isOnThisDay =
-            entryDate.getMonth() === today.getMonth() && entryDate.getDate() === today.getDate()
-          return isOnThisDay
+            entryDate.getMonth() === today.getMonth() &&
+            entryDate.getDate() === today.getDate();
+          return isOnThisDay;
         })
         .sort((a, b) => {
-          const aDate = new Date(a.date_created_by_author)
-          const bDate = new Date(b.date_created_by_author)
-          return bDate - aDate
+          const aDate = new Date(a.date_created_by_author);
+          const bDate = new Date(b.date_created_by_author);
+          return bDate - aDate;
         }),
-    [entries, showOnlyPublic],
-  )
+    [entries, showOnlyPublic]
+  );
 
   return (
     <Fragment>
       <Col xs={12} className='p-0'>
-        <Header fill='var(--quinaryColor)' href={headerLink ? RouteMap.ENTRIES_REDISCOVER : null}>
+        <Header
+          fill='var(--quinaryColor)'
+          href={headerLink ? RouteMap.ENTRIES_REDISCOVER : null}
+        >
           Rediscover This Day
         </Header>
       </Col>
@@ -48,18 +59,18 @@ export const EntriesRediscover = ({ entries, showOnlyPublic, height, headerLink 
         <EntryCards entries={entriesOnThisDay} containerRef={containerRef} />
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
 EntriesRediscover.propTypes = {
   entries: EntriesPropTypes,
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  headerLink: PropTypes.bool,
-}
+  headerLink: PropTypes.bool
+};
 
 EntriesRediscover.defaultProps = {
   height: 424,
-  headerLink: false,
-}
+  headerLink: false
+};
 
-export default connect(mapStateToProps)(EntriesRediscover)
+export default connect(mapStateToProps)(EntriesRediscover);
